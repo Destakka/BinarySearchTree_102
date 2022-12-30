26d47e29aca646ad76d74e6234fda8bdcc6e3b46
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-12-30 17:43:29 +0700

    add public void postorder

--- BinarySearchTree_102/Program.cs
+++ BinarySearchTree_102/Program.cs
@@ -96,7 +96,20 @@ namespace BinarySearchTree_102
                 preorder(ptr.leftchild);
                 preorder(ptr.rightchild);
             }
-
+        }
+        public void postorder (Node ptr) /*Performs the postorder traversal of the tree*/
+        {
+            if (ROOT != null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.Write(ptr.info + " ");
+            }
         }
     }
     internal class Program
