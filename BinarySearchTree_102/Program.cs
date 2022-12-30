eb514ca4455ca2d5a077ebef82341b1908608803
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-12-30 17:33:28 +0700

    add public void search

--- BinarySearchTree_102/Program.cs
+++ BinarySearchTree_102/Program.cs
@@ -40,7 +40,7 @@ namespace BinarySearchTree_102
             }
             else /*if the spesified node is not present*/
             {
-                tmp = new Node(element, null, null) /*Creates a Node*/
+                tmp = new Node(element, null, null); /*Creates a Node*/
                 if (parent ==  null) /*If the trees is empty*/
                 {
                     ROOT = tmp;
@@ -53,6 +53,20 @@ namespace BinarySearchTree_102
                 {
                     parent.rightchild = tmp;
                 }
+            }
+        }
+        public void Search (string element, ref Node parent, ref Node currentNode)
+        {
+            /*This function searches the currentNode of the specified Node as well as the current Node of its parent */
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
             }
         }
     }
